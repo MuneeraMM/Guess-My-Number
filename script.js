f143045812290d13d4970fb612fcb1c4f534a3bd
MuneeraMM <97776535+MuneeraMM@users.noreply.github.com>
2022-01-15 21:29:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score=20;
+let highscore=0;
+const displayMessage=function(message){
+    document.querySelector('.message').textContent=message;
+}
+document.querySelector('.check').addEventListener('click',function(){
+    const guess=Number(document.querySelector('.guess').value);
+    if(!guess){
+        displayMessage('No Number');
+    }else if(guess===secretNumber){
+        displayMessage('Correct Answer');
+        document.querySelector('.number').textContent=secretNumber;
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30 rem';
+    
+     if(score>highscore){
+         document.querySelector('.highscore').textContent=score;
+     }
+ }
+     else if(guess!==secretNumber){
+         if(guess>secretNumber){
+             displayMessage('Too high');
+         }
+         else if(guess<secretNumber){
+             displayMessage('Too low');
+         }
+         score=score-1;
+         document.querySelector('.score').textContent=score;
+     }
+ });
+ document.querySelector('.again').addEventListener('click',function(){
+     score=20;
+     secretNumber=Math.trunc(Math.random()*20)+1;
+     displayMessage('Start Guessing...');
+     document.querySelector('.score').textContent=score;
+     document.querySelector('.number').textContent='?';
+     document.querySelector('.guess').value='';
+     document.querySelector('body').style.backgroundColor='#222';
+     document.querySelector('.number').style.width='15rem';
+ });
+
